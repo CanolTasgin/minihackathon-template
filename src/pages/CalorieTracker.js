76d98f39d87c7a89642b9f5a07bfac3bc4880fba
CanolTasgin <canoltasgin@gmail.com>
2023-01-25 14:13:28 -0500

    Added post api and alert message

--- src/pages/CalorieTracker.js
+++ src/pages/CalorieTracker.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Alert } from "react-bootstrap";
 
 function CalorieTracker() {
   const [foodData, setFoodData] = useState({});
+  const [selectedFood, setSelectedFood] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
 
   useEffect(() => {
     axios
@@ -11,13 +16,43 @@ function CalorieTracker() {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleSubmit = () => {
+    axios
+      .post("http://localhost:5000/add_data", { food_name: selectedFood })
+      .then((res) => {
+        setAlertType("success");
+        setAlertMessage("Food added successfully");
+        setShowAlert(true);
+      })
+      .catch((err) => {
+        setAlertType("danger");
+        setAlertMessage("Failed to add food");
+        setShowAlert(true);
+      });
+  };
+
   return (
     <div>
-      {Object.keys(foodData).map((foodName) => (
-        <div key={foodName}>
-          <p>{foodName}</p>
-        </div>
-      ))}
+      {showAlert && (
+        <Alert
+          variant={alertType}
+          onClose={() => setShowAlert(false)}
+          dismissible
+        >
+          {alertMessage}
+        </Alert>
+      )}
+      <select
+        value={selectedFood}
+        onChange={(e) => setSelectedFood(e.target.value)}
+      >
+        {Object.keys(foodData).map((foodName) => (
+          <option key={foodName} value={foodName}>
+            {foodName}
+          </option>
+        ))}
+      </select>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
